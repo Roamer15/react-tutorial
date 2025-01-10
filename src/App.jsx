557a9d33de673b945e058801f3404e37ea826220
Roamer15 <ianseeksbeta@gmail.com>
2025-01-10 21:41:25 +0100

    props with react(props, defaultprops and propTypes)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import Card from './Card.JSX'
+import Student from './Student.jsx'
 
 function App() {
  
   return(
     <>
-    <Card/>
-    <Card/>
-    <Card/>
+       <Student name ="Spongebob" age={30} isStudent={true}/>
+       <Student name ="Patrick" age={42} isStudent={false}/>
+       <Student name ="MrCrabs" age={54} isStudent={false}/>
+       <Student name ="Sandy" age={27} isStudent={true}/>
+       <Student />
     </>
+  
   );
 }
 
